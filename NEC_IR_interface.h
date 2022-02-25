d426d3c9f056af411ca6a7fb4088f376ae477355
Mohamed Mokhtar <mmokhtar761@gmail.com>
2022-02-25 13:07:27 +0200

    V02-> A better optimized code <all time timer bug>

--- NEC_IR_interface.h
+++ NEC_IR_interface.h
@@ -1,16 +1,17 @@
 /*************************************************************************/
 /* Author        : Mohamed Mokhtar Abd-Elaziz                            */
 /* File          : NEC_IR_INTERFACE_H                                    */
-/*Description    : An educational driver file for IR reciever based      */
+/*Description    : An educational driver file for IR receiver based      */
 /*                 on NEC standard.                                      */
 /* Date          : 22 02 2022                                            */
-/* Version       : V1.1-> Raw code. (Algorithm A) Essential tests        */
+/* Version       : V02-> A better optimized code                         */
 /* GitHub        : https://github.com/mmokhtar761                        */
 /*************************************************************************/
 #ifndef NEC_IR_INTERFACE_H
 #define NEC_IR_INTERFACE_H
 
 // --> 1 - You first must set the required actionTaking function
+    /*This Func is called after each successful data & addrerss detection*/
 void NECIRR_voidSetActionFunc (void (*Local_fnPtr)(u8 Local_u8Address , u8 Local_u8Data));
 /*----------------------------------------------------------------------------*/
 // --> 2 - THIS FUNC MUST BE CALLED ON EACH FALLING EDGE INTERRUPT OF INPUT PIN
@@ -24,5 +25,6 @@ void NECIRR_voidHandlelFaillingEdge (void);
 #define NECIRR_ERROR_BAD_TIME  2
 #define NECIRR_BAD_DATA_BIT    4
 #define NECIRR_BAD_ADDRESS_BIT 8
+/******************************************************************************/
 
 #endif  /*NEC_IR_INTERFACE_H*/
